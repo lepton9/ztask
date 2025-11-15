f08d42e1513eeabb21cbc9f751e395ec03b26c87
lepton <tonihirvonen00@gmail.com>
2025-11-15 21:53:00 +0200

    basic task structs

---
+++ src/task.zig
@@ -0,0 +1,39 @@
+const std = @import("std");
+
+pub const Task = struct {
+    name: []const u8,
+    trigger: ?Trigger = null,
+    jobs: []Job = undefined,
+
+    pub fn init(gpa: std.mem.Allocator, name: []const u8) !*Task {
+        const task = try gpa.create(Task);
+        task.* = .{
+            .name = try gpa.dupe(u8, name),
+        };
+        return task;
+    }
+
+    pub fn deinit(task: *Task, gpa: std.mem.Allocator) void {
+        gpa.free(task.name);
+        gpa.destroy(task);
+    }
+};
+
+pub const Trigger = union {
+    watch: struct {
+        type: enum { Dir, File },
+        path: []const u8,
+    },
+};
+
+pub const Job = struct {
+    name: []const u8,
+    steps: []Step = undefined,
+    deps: ?[]*const Job = null,
+};
+
+pub const StepKind = enum { Command };
+pub const Step = struct {
+    kind: StepKind,
+    value: []const u8,
+};
