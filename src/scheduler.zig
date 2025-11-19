00bfe70aefc12611090de9d55a6414f00ffb978a
lepton <tonihirvonen00@gmail.com>
2025-11-19 19:28:31 +0200

    cycle detection tests

--- src/scheduler.zig
+++ src/scheduler.zig
@@ -164,11 +164,98 @@ fn Node(comptime T: type) type {
         /// Nodes that depend on this node
         dependents: std.ArrayList(*@This()),
         /// Number of dependencies not yet finished
-        remaining_deps: usize,
-        scheduler: *Scheduler,
+        remaining_deps: usize = 0,
+        scheduler: *Scheduler = undefined,
 
         pub fn deinit(self: *@This(), gpa: std.mem.Allocator) void {
             self.dependents.deinit(gpa);
         }
+
+        fn getDependents(self: @This()) []*@This() {
+            return self.dependents.items;
+        }
     };
+}
+
+test "index_of_node" {
+    const NodeT = Node(usize);
+    const count = 5;
+    var ar: [count]usize = .{ 1, 2, 3, 4, 5 };
+    const gpa = std.testing.allocator;
+    var nodes = try gpa.alloc(NodeT, count);
+    defer {
+        for (nodes) |*node| node.deinit(gpa);
+        gpa.free(nodes);
+    }
+    for (0..count) |i| nodes[i] = .{
+        .ptr = &ar[i],
+        .dependents = try std.ArrayList(*NodeT).initCapacity(gpa, 2),
+    };
+    for (0..count) |i| {
+        try std.testing.expect(indexOfNode(usize, &ar, &ar[i]) == i);
+        try std.testing.expect(indexOfNode(NodeT, nodes, &nodes[i]) == i);
+    }
+}
+
+test "no_cycle" {
+    const NodeT = Node(usize);
+    const count = 5;
+    var ar: [count]usize = .{ 1, 2, 3, 4, 5 };
+    const gpa = std.testing.allocator;
+    var nodes = try gpa.alloc(NodeT, count);
+    defer {
+        for (nodes) |*node| node.deinit(gpa);
+        gpa.free(nodes);
+    }
+    for (0..count) |i| nodes[i] = .{
+        .ptr = &ar[i],
+        .dependents = try std.ArrayList(*NodeT).initCapacity(gpa, 1),
+    };
+
+    // No dependencies
+    try std.testing.expect(
+        try detectCycle(NodeT, gpa, nodes, NodeT.getDependents) == false,
+    );
+
+    // Every node depends on the last one
+    for (0..count - 1) |i| {
+        nodes[i].dependents.appendAssumeCapacity(&nodes[i + 1]);
+    }
+    try std.testing.expect(
+        try detectCycle(NodeT, gpa, nodes, NodeT.getDependents) == false,
+    );
+}
+
+test "cycle_detected" {
+    const NodeT = Node(usize);
+    const count = 5;
+    var ar: [count]usize = .{ 1, 2, 3, 4, 5 };
+    const gpa = std.testing.allocator;
+    var nodes = try gpa.alloc(NodeT, count);
+    defer {
+        for (nodes) |*node| node.deinit(gpa);
+        gpa.free(nodes);
+    }
+    for (0..count) |i| nodes[i] = .{
+        .ptr = &ar[i],
+        .dependents = try std.ArrayList(*NodeT).initCapacity(gpa, 2),
+    };
+
+    // Nodes depend on each other
+    nodes[0].dependents.appendAssumeCapacity(&nodes[1]);
+    nodes[1].dependents.appendAssumeCapacity(&nodes[0]);
+    try std.testing.expect(
+        try detectCycle(NodeT, gpa, nodes, NodeT.getDependents) == true,
+    );
+    _ = nodes[0].dependents.pop();
+    _ = nodes[1].dependents.pop();
+
+    // Full cycle
+    for (0..count - 1) |i| {
+        nodes[i].dependents.appendAssumeCapacity(&nodes[i + 1]);
+    }
+    nodes[count - 1].dependents.appendAssumeCapacity(&nodes[0]);
+    try std.testing.expect(
+        try detectCycle(NodeT, gpa, nodes, NodeT.getDependents) == true,
+    );
 }
