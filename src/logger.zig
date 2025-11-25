473da92f294d10bb468a731f3bf1ca90784e5565
lepton <tonihirvonen00@gmail.com>
2025-11-25 23:01:28 +0200

    logger documentation and use file writer interface

--- src/logger.zig
+++ src/logger.zig
@@ -23,6 +23,7 @@ pub const JobRunMetadata = struct {
     status: JonRunStatus = .pending,
 };
 
+/// Logger for task and job execution logs and metadata
 pub const RunLogger = struct {
     gpa: std.mem.Allocator,
     base_path: []const u8,
@@ -47,6 +48,7 @@ pub const RunLogger = struct {
         self.gpa.free(self.run_path);
     }
 
+    /// Write task metadata to a JSON file
     pub fn logTaskMetadata(self: *RunLogger, meta: *TaskRunMetadata) !void {
         const file_path = try std.fs.path.join(self.gpa, &.{ self.run_path, "meta.json" });
         defer self.gpa.free(file_path);
@@ -55,6 +57,7 @@ pub const RunLogger = struct {
         try writeFile(file_path, json);
     }
 
+    /// Write job metadata to a JSON file
     pub fn logJobMetadata(self: *RunLogger, meta: *JobRunMetadata) !void {
         const file_path = try std.fs.path.join(
             self.gpa,
@@ -66,17 +69,21 @@ pub const RunLogger = struct {
         try writeFile(file_path, json);
     }
 
+    /// Record the initial state of the task in a metadata file
     pub fn startTask(self: *RunLogger, meta: *TaskRunMetadata) !void {
         try std.fs.cwd().makePath(self.run_path);
         meta.start_time = std.time.timestamp();
         try self.logTaskMetadata(meta);
     }
 
+    /// Record the final status of the task in a metadata file
     pub fn endTask(self: *RunLogger, meta: *TaskRunMetadata) !void {
         meta.end_time = std.time.timestamp();
         try self.logTaskMetadata(meta);
     }
 
+    /// Record the initial state of the job in a metadata file
+    /// Creates the necessary paths and log files for the job
     pub fn startJob(self: *RunLogger, meta: *JobRunMetadata) !void {
         const cwd = std.fs.cwd();
         const dir = try std.fs.path.join(
@@ -96,11 +103,13 @@ pub const RunLogger = struct {
         try self.logJobMetadata(meta);
     }
 
+    /// Record the final state of the job in a metadata file
     pub fn endJob(self: *RunLogger, meta: *JobRunMetadata) !void {
         meta.end_time = std.time.timestamp();
         try self.logJobMetadata(meta);
     }
 
+    /// Add content to the end of the job log file
     pub fn appendJobLog(self: *RunLogger, meta: *JobRunMetadata, content: []const u8) !void {
         const file_path = try std.fs.path.join(
             self.gpa,
@@ -109,21 +118,24 @@ pub const RunLogger = struct {
         defer self.gpa.free(file_path);
         var file = try std.fs.cwd().openFile(file_path, .{ .mode = .write_only });
         defer file.close();
-        try file.seekFromEnd(0);
-
-        try file.writeAll(content);
+        var writer = file.writer(&.{});
+        try writer.seekTo(try file.getEndPos());
+        try writer.interface.writeAll(content);
     }
 };
 
+/// Encodes value to a JSON string
 fn toJson(gpa: std.mem.Allocator, value: anytype) ![]u8 {
     var out: std.io.Writer.Allocating = .init(gpa);
     try std.json.Stringify.value(value, .{ .whitespace = .indent_2 }, &out.writer);
     return try out.toOwnedSlice();
 }
 
+/// Write all the content to the file
 fn writeFile(path: []const u8, content: []const u8) !void {
     const cwd = std.fs.cwd();
     var file = try cwd.createFile(path, .{ .truncate = false });
     defer file.close();
-    try file.writeAll(content);
+    var writer = file.writer(&.{});
+    try writer.interface.writeAll(content);
 }
