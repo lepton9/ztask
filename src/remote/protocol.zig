43204681d9bee3239390c00e37c2f478d11e4bf8
lepton <tonihirvonen00@gmail.com>
2025-12-03 13:06:27 +0200

    reading tcp messages

--- src/remote/protocol.zig
+++ src/remote/protocol.zig
@@ -13,12 +13,46 @@ pub const MsgType = enum(u8) {
 /// Send message to server
 ///
 /// [[4 bytes: length N]] [[1 byte: msg type]] [[N-1 bytes: payload]]
-pub fn sendFrame(stream: *std.net.Stream, buffer: []const u8, payload: []const u8) !void {
+pub fn sendFrame(stream: *std.net.Stream, buffer: []u8, msg: []const u8) !void {
     var writer = stream.writer(buffer);
     var header: [4]u8 = undefined;
-    std.mem.writeInt(u32, &header, @intCast(payload.len), .little);
+    std.mem.writeInt(u32, &header, @intCast(msg.len), .little);
     try writer.interface.writeVecAll(&.{
         .{header},
-        .{payload},
+        .{msg},
     });
 }
+
+pub fn readFrame(gpa: std.mem.Allocator, stream: *std.net.Stream) ![]u8 {
+    var reader: std.Io.Reader = stream.reader(&.{}).interface();
+    var header: [4]u8 = undefined;
+    reader.readSliceShort(&header);
+    const len = std.mem.readInt(u32, &header, .little);
+    if (len == 0) return error.InvalidFrame;
+    var buf = try gpa.alloc(u8, len);
+    try reader.readSliceAll(&buf);
+    return buf;
+}
+
+pub fn beginPayload(gpa: std.mem.Allocator, msg_type: MsgType) !std.ArrayList(u8) {
+    var buf = try std.ArrayList(u8).initCapacity(gpa, 1);
+    buf.appendAssumeCapacity(gpa, @intFromEnum(msg_type));
+    return buf;
+}
+
+pub const Register = struct {
+    hostname: []const u8,
+
+    pub fn encode(self: Register, gpa: std.mem.Allocator) ![]u8 {
+        var buf = try beginPayload(gpa, .register);
+        try buf.appendSlice(gpa, self.hostname);
+        return buf.toOwnedSlice();
+    }
+
+    pub fn decode(msg: []const u8) !Register {
+        if (msg.len < 1) return error.InvalidPayload;
+        if (@as(MsgType, @enumFromInt(msg[0])) != .register) return error.InvalidMsgType;
+        const hostname = msg[1..];
+        return .{ .hostname = hostname };
+    }
+};
