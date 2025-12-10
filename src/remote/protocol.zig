0557a8771c7817a1a9a6e8e90893dbb1604a0763
lepton <tonihirvonen00@gmail.com>
2025-12-10 22:15:47 +0200

    run and cancel job message types

--- src/remote/protocol.zig
+++ src/remote/protocol.zig
@@ -47,3 +47,54 @@ pub const RegisterMsg = struct {
         return .{ .hostname = hostname };
     }
 };
+
+pub const RunJobMsg = struct {
+    job_id: u64,
+    steps: []const u8, // JSON
+
+    pub fn serialize(self: RunJobMsg, gpa: std.mem.Allocator) ![]u8 {
+        var id: [8]u8 = undefined;
+        std.mem.writeInt(u64, id[0..8], self.job_id, .little);
+        var buf = try beginPayload(gpa, .run_job);
+        try buf.appendSlice(gpa, id);
+        try buf.appendSlice(gpa, self.steps);
+        return buf.toOwnedSlice(gpa);
+    }
+
+    pub fn parse(msg: []const u8) !RunJobMsg {
+        if (msg.len < 8) return error.InvalidPayload;
+        const job_id = readU64Le(msg);
+        const steps = msg[8..];
+        return .{ .job_id = job_id, .steps = steps };
+    }
+};
+
+pub const CancelJobMsg = struct {
+    job_id: u64,
+
+    pub fn serialize(self: RunJobMsg, gpa: std.mem.Allocator) ![]u8 {
+        var id: [8]u8 = undefined;
+        std.mem.writeInt(u64, id[0..8], self.job_id, .little);
+        var buf = try beginPayload(gpa, .cancel_job);
+        try buf.appendSlice(gpa, id);
+        return buf.toOwnedSlice(gpa);
+    }
+
+    pub fn parse(msg: []const u8) !RunJobMsg {
+        if (msg.len < 8) return error.InvalidPayload;
+        const job_id = readU64Le(msg);
+        return .{ .job_id = job_id };
+    }
+};
+
+/// Reads a u64 little endian integer from the slice
+/// Slice length must be over 8 bytes
+fn readU64Le(s: []const u8) u64 {
+    return std.mem.readInt(u64, s[0..8], .little);
+}
+
+/// Reads a u32 little endian integer from the slice
+/// Slice length must be over 4 bytes
+fn readU32Le(s: []const u8) u32 {
+    return std.mem.readInt(u32, s[0..4], .little);
+}
