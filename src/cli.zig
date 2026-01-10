5347798c4b4138e4f82e746b43bf038653c04fbf
lepton <tonihirvonen00@gmail.com>
2026-01-10 23:25:01 +0200

    use function pointers for commands

--- src/cli.zig
+++ src/cli.zig
@@ -1,5 +1,6 @@
-pub const zcli = @import("zcli");
 pub const std = @import("std");
+pub const zcli = @import("zcli");
+const run = @import("run.zig");
 const options = @import("build_options");
 
 /// Cli configuration
@@ -36,6 +37,7 @@ const commands = &[_]zcli.Cmd{
             },
             runner_n_option,
         },
+        .action = cmdRunFn,
     },
     .{
         .name = "runner",
@@ -73,11 +75,13 @@ const commands = &[_]zcli.Cmd{
             },
             runner_n_option,
         },
+        .action = cmdRunnerFn,
     },
     .{
         .name = "list",
         .desc = "List all the tasks",
         .options = &[_]zcli.Opt{},
+        .action = cmdListFn,
     },
     .{
         .name = "completion",
@@ -85,6 +89,7 @@ const commands = &[_]zcli.Cmd{
         .positionals = &[_]zcli.PosArg{
             .{ .name = "shell", .desc = "Shell name", .required = true },
         },
+        .action = cmdCompletionFn,
     },
 };
 
@@ -94,3 +99,99 @@ const runner_n_option: zcli.Opt = .{
     .desc = "Maximum amount of runners active",
     .arg = .{ .name = "INT", .type = .Int },
 };
+
+/// Write all the data to stdout
+fn write_to_stdout(data: []const u8) !void {
+    var buffer: [1024]u8 = undefined;
+    var writer = std.fs.File.stdout().writer(&buffer);
+    const stdout = &writer.interface;
+    try stdout.writeAll(data);
+    try stdout.flush();
+}
+
+/// Generate shell completions
+fn generate_completion(
+    cli: *zcli.Cli,
+    comptime spec: *const zcli.CliApp,
+) !noreturn {
+    var buf: [8096]u8 = undefined;
+    const shell = cli.find_positional("shell") orelse unreachable;
+    const script = try zcli.complete.getCompletion(
+        &buf,
+        spec,
+        spec.config.name.?,
+        shell.value,
+    );
+    try write_to_stdout(script);
+    std.process.exit(0);
+}
+
+/// Context given to command functions
+const Ctx = struct {
+    gpa: std.mem.Allocator,
+    cli: *zcli.Cli,
+};
+
+/// Handle run command
+fn cmdRunFn(ptr: *anyopaque) !void {
+    const ctx: *Ctx = @ptrCast(@alignCast(ptr));
+    var cli = ctx.cli;
+
+    var opts: run.RunOptions = .{
+        .path = if (cli.find_opt("path")) |o| o.value.?.string else null,
+        .id = if (cli.find_opt("id")) |o| o.value.?.string else null,
+    };
+    if (cli.find_opt("runners")) |opt| {
+        const n = opt.value.?.int;
+        if (n <= 0 or n > run.MAX_RUNNERS_N) return error.InvalidRunnerAmount;
+        opts.runners_n = @intCast(n);
+    }
+    return run.runTask(ctx.gpa, opts);
+}
+
+/// Handle runner command
+fn cmdRunnerFn(ptr: *anyopaque) !void {
+    const ctx: *Ctx = @ptrCast(@alignCast(ptr));
+    var cli = ctx.cli;
+
+    const name = cli.find_opt("name") orelse unreachable;
+    const addr = cli.find_opt("address");
+    const port: ?u16 = blk: {
+        if (cli.find_opt("port")) |p| {
+            const port = p.value.?.int;
+            if (port < 0) return error.InvalidPort;
+            break :blk @truncate(@as(u64, @intCast(port)));
+        } else break :blk null;
+    };
+    var opts: run.AgentOptions = .{
+        .name = name.value.?.string,
+    };
+    if (addr) |a| opts.addr = a.value.?.string;
+    if (port) |p| opts.port = p;
+    if (cli.find_opt("runners")) |opt| {
+        const n = opt.value.?.int;
+        if (n <= 0 or n > run.MAX_RUNNERS_N) return error.InvalidRunnerAmount;
+        opts.runners_n = @intCast(n);
+    }
+    return try run.runAgent(ctx.gpa, opts);
+}
+
+/// Handle list command
+fn cmdListFn(ptr: *anyopaque) !void {
+    const ctx: *Ctx = @ptrCast(@alignCast(ptr));
+    return try run.listTasks(ctx.gpa);
+}
+
+/// Handle completion command
+fn cmdCompletionFn(ptr: *anyopaque) !void {
+    const ctx: *Ctx = @ptrCast(@alignCast(ptr));
+    return try generate_completion(ctx.cli, &cli_spec);
+}
+
+/// Handle parsed cli and call the command function
+pub fn handleArgs(gpa: std.mem.Allocator, cli: *zcli.Cli) !void {
+    const cmd = cli.cmd orelse return try run.runTui(gpa, .{});
+    const cmdFn = cmd.exec orelse return;
+    var ctx: Ctx = .{ .gpa = gpa, .cli = cli };
+    try cmdFn(&ctx);
+}
--- src/main.zig
+++ src/main.zig
@@ -8,77 +8,9 @@ pub const std_options: std.Options = .{
     .log_scope_levels = &[_]std.log.ScopeLevel{
         .{ .scope = .parser, .level = .info },
         .{ .scope = .tokenizer, .level = .info },
+        .{ .scope = .runner, .level = .info },
     },
 };
-
-fn write_to_stdout(data: []const u8) !void {
-    var buffer: [1024]u8 = undefined;
-    var writer = std.fs.File.stdout().writer(&buffer);
-    const stdout = &writer.interface;
-    try stdout.writeAll(data);
-    try stdout.flush();
-}
-
-fn generate_completion(
-    cli: *zcli.Cli,
-    comptime spec: *const zcli.CliApp,
-) !noreturn {
-    var buf: [8096]u8 = undefined;
-    const shell = cli.find_positional("shell") orelse unreachable;
-    const script = try zcli.complete.getCompletion(
-        &buf,
-        spec,
-        spec.config.name.?,
-        shell.value,
-    );
-    try write_to_stdout(script);
-    std.process.exit(0);
-}
-
-fn handleArgs(
-    gpa: std.mem.Allocator,
-    cli: *zcli.Cli,
-    comptime spec: *const zcli.CliApp,
-) !void {
-    const cmd = cli.cmd orelse return try run.runTui(gpa, .{});
-    if (std.mem.eql(u8, cmd.name, "run")) {
-        var opts: run.RunOptions = .{
-            .path = if (cli.find_opt("path")) |o| o.value.?.string else null,
-            .id = if (cli.find_opt("id")) |o| o.value.?.string else null,
-        };
-        if (cli.find_opt("runners")) |opt| {
-            const n = opt.value.?.int;
-            if (n <= 0 or n > run.MAX_RUNNERS_N) return error.InvalidRunnerAmount;
-            opts.runners_n = @intCast(n);
-        }
-        return run.runTask(gpa, opts);
-    } else if (std.mem.eql(u8, cmd.name, "runner")) {
-        const name = cli.find_opt("name") orelse unreachable;
-        const addr = cli.find_opt("address");
-        const port: ?u16 = blk: {
-            if (cli.find_opt("port")) |p| {
-                const port = p.value.?.int;
-                if (port < 0) return error.InvalidPort;
-                break :blk @truncate(@as(u64, @intCast(port)));
-            } else break :blk null;
-        };
-        var opts: run.AgentOptions = .{
-            .name = name.value.?.string,
-        };
-        if (addr) |a| opts.addr = a.value.?.string;
-        if (port) |p| opts.port = p;
-        if (cli.find_opt("runners")) |opt| {
-            const n = opt.value.?.int;
-            if (n <= 0 or n > run.MAX_RUNNERS_N) return error.InvalidRunnerAmount;
-            opts.runners_n = @intCast(n);
-        }
-        return try run.runAgent(gpa, opts);
-    } else if (std.mem.eql(u8, cmd.name, "list")) {
-        return try run.listTasks(gpa);
-    } else if (std.mem.eql(u8, cmd.name, "completion")) {
-        return try generate_completion(cli, spec);
-    }
-}
 
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
@@ -87,5 +19,5 @@ pub fn main() !void {
     // Parse cli args
     const cli: *zcli.Cli = try zcli.parseArgs(allocator, &cli_zig.cli_spec);
     defer cli.deinit(allocator);
-    try handleArgs(allocator, cli, &cli_zig.cli_spec);
+    try cli_zig.handleArgs(allocator, cli);
 }
